1973ee94feebca96670d21447d8d33f7f67df77c
Derek Hauac <hauacderek@gmail.com>
2021-02-11 20:46:26 +0800

    initial commit

---
+++ app/src/main/java/mcm/edu/ph/hauac_calculator/MainActivity.java
@@ -0,0 +1,36 @@
+package mcm.edu.ph.hauac_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button, button2, button7, button8, button9, button10, button11, button12, button19, button20, button21, button22, button29, button30, button31, button32, button33, button34, button36, button37;
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+        button11 = findViewById(R.id.button11);
+        button12 = findViewById(R.id.button12);
+        button19 = findViewById(R.id.button19);
+        button20 = findViewById(R.id.button20);
+        button21 = findViewById(R.id.button21);
+        button22 = findViewById(R.id.button22);
+        button29 = findViewById(R.id.button29);
+        button30 = findViewById(R.id.button30);
+        button31 = findViewById(R.id.button31);
+        button32 = findViewById(R.id.button32);
+        button33 = findViewById(R.id.button33);
+        button34 = findViewById(R.id.button34);
+        button36 = findViewById(R.id.button36);
+        button37 = findViewById(R.id.button37);
+    }
+}
